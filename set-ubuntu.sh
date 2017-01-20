530a47197d49e6d5746908ed206ab00126662a35
Likemilk <dydwls121200@gmail.com>
2017-01-21 00:07:00 +0900

    auto push

--- set-ubuntu.sh
+++ set-ubuntu.sh
@@ -1,9 +1,15 @@
 #!bin/bash
 
+#Init package installations 
 sudo apt-get install -y tmux zsh vim git gcc make cmake python-pip python3-pip
 #dot vimrc install packages
 sudo apt-get install -y vim-gtk ack-grep ctags
+pip install --upgrade pip
+pip3 install --upgrade pip
 
+
+
+#zsh setting
 chsh -s `which zsh`
 url -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 zsh
@@ -21,11 +27,7 @@ source ~/.zshrc
 
 
 
-
-
-pip install --upgrade pip
-pip3 install --upgrade pip
-
+# nodejs installation
 sudo mkdir /usr/local/develop
 sudo chown -R likemilk:likemilk develop
 mkdir /usr/local/develop/language
@@ -35,11 +37,18 @@ tar xvf node-v6.9.4-linux-x64.tar.xz
 mv node-v6.9.4-linux-x64.tar.xz /usr/local/develop/language/node-6.9.4
 
 
+#markdown reader 
+# pip base on python2.7 can't install grip.
+pip3 install grip 
+#test 
+#grip ~/ubuntu-config/README.md
+
+
+
 
 #setting tmux
 #https://github.com/tony/tmux-config
 #(all setting is following this git page)
-
 git clone https://github.com/tony/tmux-config.git ~/.tmux
 ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
 cd ~/.tmux
@@ -64,7 +73,6 @@ sudo chmod +x /usr/local/bin/tmux-mem-cpu-load
 
 
 
-
 #setting powerline fonts
 git clone https://github.com/powerline/fonts
 cd fonts
@@ -72,26 +80,24 @@ cd fonts
 cd ..
 rm -rf fonts
 
+
 #setting tmuxp
 #It is save your workspace(detail is referred to this page 
 #http://tmuxp.git-pull.com/en/latest/quickstart.html
 #this page is tutorial about tmuxp
 pip install tmuxp
 
+
+
+
+
 #dot vimrc config 
 #https://github.com/humiaozuzu/dot-vimrc
-
 git clone git://github.com/humiaozuzu/dot-vimrc.git ~/.vim
 ln -s ~/.vim/vimrc ~/.vimrc
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vi ubuntu-config/dot-vimsetting
 
-#markdown reader 
-# pip base on python2.7 can't install grip.
-pip3 install grip 
-
-#test 
-#grip ~/ubuntu-config/README.md
 
 
 
